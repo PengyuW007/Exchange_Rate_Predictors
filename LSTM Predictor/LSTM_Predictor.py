099f684d42932fdecbda34b98ba5c0141ab405d6
Pengyu Wang <pwang2@laurentian.ca>
2024-04-12 16:58:24 -0400

    Initial commit

---
+++ LSTM Predictor/LSTM_Predictor.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.layers import Dense
+
+dataset_train = pd.read_csv('CAD_CNY Historical Data_20230311_20240411.csv')
+training_set = dataset_train.iloc[:, 1:2].values
+
+sc = MinMaxScaler(feature_range=(0, 1))
+training_set_scaled = sc.fit_transform(training_set)
+
+X_train = []
+y_train = []
+# Keep in mind always change index of i within range
+for i in range(60, 284):
+    X_train.append(training_set_scaled[i - 60:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+
+model.add(Dense(units=1))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+model.fit(X_train, y_train, epochs=100, batch_size=4)
+
+dataset_test = pd.read_csv('CAD_CNY Historical Data_20240312_20240412.csv')
+real_stock_price = dataset_test.iloc[:, 1:2].values
+
+dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis=0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = inputs.reshape(-1, 1)
+inputs = sc.transform(inputs)
+X_test = []
+for i in range(60, 85):
+    X_test.append(inputs[i - 60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted_stock_price = model.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+plt.plot(real_stock_price, color='black', label='Real CAD/CNY Exchange Rate')
+plt.plot(predicted_stock_price, color='green', label='Predicted CAD/CNY Exchange Rate')
+plt.title('CAD/CNY Exchange Rate Prediction')
+plt.xlabel('Time')
+plt.ylabel('CAD/CNY Exchange Rate')
+plt.legend()
+plt.show()
